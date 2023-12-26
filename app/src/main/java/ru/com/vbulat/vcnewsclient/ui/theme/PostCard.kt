33d8c3bda9c6764f1ef6424c154110cd46bc3b71
VB <Vladimir.Bulat@gmail.com>
2023-12-26 14:58:37 +0300

    Lesson 01

---
+++ app/src/main/java/ru/com/vbulat/vcnewsclient/ui/theme/PostCard.kt
@@ -0,0 +1,83 @@
+package ru.com.vbulat.vcnewsclient.ui.theme
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ru.com.vbulat.vcnewsclient.R
+
+@Composable
+fun PostCard(){
+    Card {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Image(
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape),
+                painter = painterResource(id = R.drawable.post_comunity_thumbnail),
+                contentDescription = null,
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column (
+                modifier = Modifier.weight(1f)
+            ){
+                Text(
+                    text = "dev/null",
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = "14:00",
+                    color = MaterialTheme.colorScheme.secondary
+                )
+            }
+
+            Icon(
+                imageVector = Icons.Rounded.MoreVert,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.secondary
+            )
+        }
+    }
+}
+
+@Preview()
+@Composable
+fun PostCardPreviewLight(){
+    VcNewsClientTheme(darkTheme = false) {
+        PostCard()
+    }
+}
+
+@Preview()
+@Composable
+fun PostCardPreviewDarck(){
+    VcNewsClientTheme(darkTheme = true) {
+        PostCard()
+    }
+}
